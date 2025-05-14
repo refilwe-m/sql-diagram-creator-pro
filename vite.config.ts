85849bdec816f4419a7787144b79059962540f2a
refilwe-m <refilwe.dev@gmail.com>
2025-05-15 00:18:32 +0200

    GH Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/sql-diagram-creator-pro/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
